30ee236815d2118fbb548c1bc3369b6cb1e60ef4
SrFebry <febryanmdp@mhs.mdp.ac.id>
2024-03-01 19:24:22 +0700

    Main dart dan HomeScreen API 2
    melanjutkan membuat tampilan homescreen dan detailscreen (menampilkan)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,75 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
+import 'package:pilem/services/api_service.dart';
+import 'detail_screen.dart';
 
-class DetailScreen extends StatelessWidget {
-  final Movie movie;
-  const DetailScreen({super.key, required this.movie});
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final ApiService _apiService = ApiService();
+  List<Movie> _allMovies = [];
+  List<Movie> _trendingMovies = [];
+  List<Movie> _popularMovies = [];
+  @override
+  void initState() {
+    super.initState();
+    _loadMovies();
+  }
+
+  Future<void> _loadMovies() async {
+    final List<Map<String, dynamic>> allMoviesData =
+        await _apiService.getAllMovies();
+    final List<Map<String, dynamic>> trendingMoviesData =
+        await _apiService.getTrendingMovies();
+    final List<Map<String, dynamic>> popularMoviesData =
+        await _apiService.getPopularMovies();
+    setState(() {
+      _allMovies = allMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _trendingMovies =
+          trendingMoviesData.map((e) => Movie.fromJson(e)).toList();
+      _popularMovies = popularMoviesData.map((e) => Movie.fromJson(e)).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(movie.title),
+        title: const Text('Pilem'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network('https://image.tmdb.org/t/p/w500${movie.backdropPath}',
-          height: 300,
-          width: double.infinity,
-          fit: BoxFit.cover,
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Overview',
-            style: TextStyle(
-              fontSize: 20, 
-              fontWeight: FontWeight.bold,                  
-            ),
-          ),
-          SizedBox(height: 10),
-          Text(movie.overview),
-          SizedBox(height: 20),
-          
-          Row(
-            children: [
-              Icon(
-                Icons.calendar_month, 
-                color: Colors.blue,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Release Date : ', 
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-                  SizedBox(width: 10),
-                  Text(movie.releaseDate),                 
-            ],
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildMoviesList('All Movies', _allMovies),
+            _buildMoviesList('Trending Movies', _trendingMovies),
+            _buildMoviesList('Popular Movies', _popularMovies),
+          ],
+        ),
+      ),
+    );
+  }
 
-          Row(
-            children: [
-              Icon(
-                Icons.star, 
-                color: Colors.amber,
-              ),
-              const SizedBox(width: 10),
-              const Text(
-                'Rating : ', 
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-                  SizedBox(width: 10),
-                  Text(movie.voteAverage.toString()),
-            ],
+  Widget _buildMoviesList(String title, List<Movie> movies) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
           ),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: 200,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: movies.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Movie movie = movies[index];
+              return GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailScreen(movie: movie),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Image.network(
+                        'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                        height: 150,
+                        width: 100,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        movie.title.length > 14
+                            ? '${movie.title.substring(0, 10)}...'
+                            : movie.title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,25 +2,27 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String baseUrl = "https://api.themoviedb.org/3";
   static const String apiKey = 'eb576c13fb69a5438fbe9e2981cf7396';
 
   Future<List<Map<String, dynamic>>> getAllMovies() async {
     final response =
-        await http.get(Uri.parse('$baseUrl/movie/now_play?api_key=$apiKey'));
+        await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
+
   Future<List<Map<String, dynamic>>> getTrendingMovies() async {
-    final response =
-        await http.get(Uri.parse('$baseUrl/movie/week?api_key=$apiKey'));
+    final response = await http
+        .get(Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
+
   Future<List<Map<String, dynamic>>> getPopularMovies() async {
     final response =
-        await http.get(Uri.parse('$baseUrl/movie/popular?api_key=$apiKey'));
+        await http.get(Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"));
     final data = json.decode(response.body);
-    return List<Map<String, dynamic>>.from(data['result']);
+    return List<Map<String, dynamic>>.from(data['results']);
   }
 }
